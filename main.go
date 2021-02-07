4f430e342d03ef0bbb2442bb98fe7d5d3ac38cd6
Mix-Liten <mixliten@gmail.com>
2021-02-07 23:01:55 +0800

    feat: use vanilla http serveMux

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 )
 
+func indexHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "this is index page")
+}
+
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "hello world")
+}
+
 func main() {
-	fmt.Println("init")
+	r := http.NewServeMux()
+	r.HandleFunc("/", indexHandler)
+	r.HandleFunc("/hello", helloHandler)
+	port := ":8080"
+	fmt.Println("init in port", port)
+	http.ListenAndServe(port, r)
 }
