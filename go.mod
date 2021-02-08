18526574e3696b75530f96a0a8518cd68eab5ce8
Mix-Liten <mixliten@gmail.com>
2021-02-08 15:45:06 +0800

    feat: add negroni tools for common middleware

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/mix-liten/golang-vanilla-http
 
 go 1.15
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/urfave/negroni v1.0.0
+)
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
 	"io"
 	"net/http"
 )
@@ -18,24 +19,43 @@ func indexHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "this is index page")
 }
 
+func errorPageHandler(w http.ResponseWriter, req *http.Request) {
+	panic("test recovery middleware...")
+	//log.Fatal("test recovery middleware...")
+	//...do something
+}
+
 func articleHandler(w http.ResponseWriter, req *http.Request) {
+	// /articles/test?p=3
 	vars := mux.Vars(req)
 	articleName := vars["name"]
 	page := req.URL.Query().Get("p")
 	ua := req.Header.Get("User-Agent")
 	io.WriteString(w, "Articles: ")
 	io.WriteString(w, articleName)
-	io.WriteString(w, ", where page: ")
+	io.WriteString(w, "\nwhere page: ")
 	io.WriteString(w, page)
-	io.WriteString(w, "user-agent: ")
+	io.WriteString(w, "\nuser-agent: ")
 	io.WriteString(w, ua)
+}
+
+func serverMiddlewareHandler(w http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
+	w.Header().Set("SERVER", "VANILLA-SERVER")
+	next(w, req)
 }
 
 func main() {
 	r := mux.NewRouter()
+	r.HandleFunc("/", indexHandler)
 	r.NotFoundHandler = http.HandlerFunc(notFoundHandler)
 	r.HandleFunc("/articles/{name}", articleHandler)
+	r.HandleFunc("/error", errorPageHandler)
+
+	n := negroni.New(negroni.NewRecovery(), negroni.NewLogger())
+	n.Use(negroni.HandlerFunc(serverMiddlewareHandler))
+	n.UseHandler(r)
+
 	port := ":8080"
 	fmt.Println("init in port", port)
-	http.ListenAndServe(port, r)
+	http.ListenAndServe(port, n)
 }
